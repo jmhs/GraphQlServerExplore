b9cba523c43eb70ddfa0b7617e892e11ba312f20
Han 2 <jmh.sng@gmail.com>
2017-11-10 20:42:07 +0800

    json server working, graph working

--- schema.js
+++ schema.js
@@ -1,3 +1,5 @@
+const axios = require("axios");
+
 //Bring in a bunch of stuff from graphql
 
 // datatypes you'll be using
@@ -10,13 +12,14 @@ const {
   GraphQLNonNull,
 } = require("graphql");
 
-
-//HardCoded data
-const customers = [
-  {id:"1", name:"John Doe", email: "jdoe@gmail.com", age:35},
-  {id:"2", name:"Steve Smith", email: "steve@gmail.com", age:25},
-  {id:"3", name:"Sara Williams", email: "sara@gmail.com", age:32},
-]
+                /*
+                //HardCoded data
+                const customers = [
+                  {id:"1", name:"John Doe", email: "jdoe@gmail.com", age:35},
+                  {id:"2", name:"Steve Smith", email: "steve@gmail.com", age:25},
+                  {id:"3", name:"Sara Williams", email: "sara@gmail.com", age:32},
+                ]
+                */
 
 //Customer Object
 // fields; define fields
@@ -47,21 +50,25 @@ const RootQuery = new GraphQLObjectType({
       },
       //resolve the request (id) by searching for id that matches
       resolve(parentValue,args){
+        /*
         for(let i = 0; i< customers.length; i++){
-          if(customers[i].id === args.id){
-            return customers[i];
-            //return current iteration that matches id
+        //   if(customers[i].id === args.id){
+        //     return customers[i];
+        //returns current iteration that matches id
+          */
+          return axios.get("http://localhost:3000/customers/"+ args.id)
+            .then(res => res.data); //fetch data from res obj
+          }
+        },
+        customers:{
+          type: new GraphQLList(CustomerType),
+          resolve(parentValue, args){
+            return axios.get("http://localhost:3000/customers/"+ args.id)
+              .then(res => res.data); //fetch data from res obj
           }
         }
       }
-    },
-    customers:{
-      type: new GraphQLList(CustomerType),
-      resolve(parentValue, args){
-        return customers;
-      }
-    }
-  }
+  
 
 });
 
