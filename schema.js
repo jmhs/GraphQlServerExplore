801ec66cd5d3a9cf39df69dc5309034ef4a9f69a
Han 2 <jmh.sng@gmail.com>
2017-11-10 19:59:05 +0800

    before adding JSON server

--- schema.js
+++ schema.js
@@ -54,7 +54,14 @@ const RootQuery = new GraphQLObjectType({
           }
         }
       }
-    }}
+    },
+    customers:{
+      type: new GraphQLList(CustomerType),
+      resolve(parentValue, args){
+        return customers;
+      }
+    }
+  }
 
 });
 
